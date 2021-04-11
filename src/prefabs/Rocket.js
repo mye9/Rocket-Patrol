f440f01461ff319cb29abe9798a4afa824804d27
mye9 <mye9@ucsc.edu>
2021-04-11 09:36:34 -0700

    7th commit

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -18,9 +18,8 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         // fire button
-        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
+        if(Phaser.Input.Keyboard.JustDown(keyF)) {
             this.isFiring = true;
-            this.sfxRocket.play();
         }
         // if fired, move up
         if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
@@ -30,5 +29,10 @@ class Rocket extends Phaser.GameObjects.Sprite {
         if(this.y <= borderUISize * 3 + borderPadding) {
             this.reset();
         }
+    }
+
+    reset(){
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }
